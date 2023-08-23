17e4ab94631e3b4055ddacc02dba26c26c8ebca5
Valentin Simois <valentincarrerasimois@gmail.com>
2023-08-23 15:08:24 -0300

    pages

--- src/app/dashboard/pages/classrooms/classrooms.component.ts
+++ src/app/dashboard/pages/classrooms/classrooms.component.ts
@@ -6,6 +6,7 @@ import { ClassroomWithStudentAndCourse } from './models';
 import { selectClassrooms } from './store/classroom.selectors';
 import { MatDialog } from '@angular/material/dialog';
 import { ClassroomDialogComponent } from './components/classroom-dialog/classroom-dialog.component';
+import { selectIsAdmin } from 'src/app/store/auth/auth.selector';
 
 @Component({
   selector: 'app-classrooms',
@@ -15,16 +16,20 @@ import { ClassroomDialogComponent } from './components/classroom-dialog/classroo
 })
 export class ClassroomsComponent implements OnInit {
 
-  displayedColumns = ['id', 'student', 'course', 'type'];
+  public isAdmin$: Observable<boolean>;
+
+  displayedColumns = ['id', 'student', 'course', 'type', 'actions'];
   classrooms$: Observable<ClassroomWithStudentAndCourse[]>;
 
   constructor(private store:Store, private matDialog:MatDialog){
-    this.classrooms$ = this.store.select(selectClassrooms)
+    this.classrooms$ = this.store.select(selectClassrooms),
+    this.isAdmin$ = this.store.select(selectIsAdmin)
   }
 
   onAdd():void{
     this.matDialog.open(ClassroomDialogComponent)
   }
+
 
   ngOnInit(): void {
     this.store.dispatch(ClassroomActions.loadClassrooms())
